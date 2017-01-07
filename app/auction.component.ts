7d9aac9e28711e27a5f687cb455f092858b89255
Steffen Hansen <stefh14@student.sdu.dk>
2017-01-07 16:13:47 +0100

    Work on http

--- app/auction.component.ts
+++ app/auction.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import * as io from 'socket.io-client';
+
+import { Auction } from './auction';
 
 @Component({
     moduleId: module.id,
     selector: 'auction',
     templateUrl: 'auction.component.html'
 })
-export class AuctionComponent implements OnInit {
+export class AuctionComponent implements OnInit, OnDestroy {
 
-    constructor() {}
+    private url: string = window.location.hostname + ":9998";
+    private socket: SocketIOClient.Socket;
+    private sub: any;
+    private ready: boolean = false;
+    private auctionInfo: Auction;
+
+    constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        
+        this.sub = this.route.params.subscribe(params => {
+            var id = +params['aId'];
+            var url = this.url + "/auction/" + id;
+            console.log(url);
+            this.socket = io.connect(url);
+            this.socket.on("auctionInfo", (info: any) => {
+                this.auctionInfo = info.json();
+            })
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 }

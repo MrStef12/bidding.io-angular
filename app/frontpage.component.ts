1280900b2573c722117205ad53d418e37b729296
Steffen Hansen <stefh14@student.sdu.dk>
2017-01-07 21:04:37 +0100

    Added interval and date format

--- app/frontpage.component.ts
+++ app/frontpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Auction } from './auction';
 import { RestService } from './rest.service';
@@ -8,16 +8,36 @@ import { RestService } from './rest.service';
     selector: 'frontpage',
     templateUrl: 'frontpage.component.html'
 })
-export class FrontpageComponent implements OnInit {
+export class FrontpageComponent implements OnInit, OnDestroy {
 
-    auctions: Auction[] = [];
-    error: boolean = false;
+    private auctions: Auction[] = [];
+    private error: boolean = false;
+    private reloadInterval: number = 2000;
+    private timerId: NodeJS.Timer;
 
     constructor(private restService: RestService) {}
 
     ngOnInit(): void {
+        this.refreshData();
+        this.timerId = setInterval(() => this.refreshData(), this.reloadInterval);
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.timerId);
+    }
+
+    refreshData(): void {
         this.restService.getAuctions()
             .then(auctions => this.auctions = auctions)
-            .catch(error => this.error = true);
+            .catch((error) => {
+                this.error = true;
+                clearInterval(this.timerId);
+                console.error("REST returned an error.",error);
+            });
+    }
+
+    public convertDate(input: string): string {
+        var date: Date = new Date(input);
+        return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
     }
 }
